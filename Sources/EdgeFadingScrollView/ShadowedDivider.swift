5002a0fdd4d78dda27ceec93e2cfff6cca406137
Manabu Nakazawa <mshibanami+git@gmail.com>
2023-08-12 14:04:12 +1000

    Fix the border is not shown properly on my low external monitor with low resolution

--- Sources/EdgeFadingScrollView/ShadowedDivider.swift
+++ Sources/EdgeFadingScrollView/ShadowedDivider.swift
@@ -9,20 +9,33 @@
 import Foundation
 import SwiftUI
 
+#if os(iOS)
+import UIKit
+#elseif os(macOS)
+import AppKit
+#endif
+
+var screenScale: CGFloat {
+#if os(iOS)
+    return UIScreen.main.scale
+#elseif os(macOS)
+    return NSScreen.main?.backingScaleFactor ?? 1.0
+#endif
+}
 struct ShadowedDivider: View {
     let shadowPosition: ShadowPosition
     let dividerColor: Color
     let shadowColor: Color
-    
+
     init(shadowPosition: ShadowPosition, dividerColor: Color, shadowColor: Color) {
         self.shadowPosition = shadowPosition
         self.dividerColor = dividerColor
         self.shadowColor = shadowColor
     }
-    
+
     var body: some View {
         Rectangle()
-            .frame(height: 0.5)
+            .frame(height: 1 / screenScale)
             .foregroundColor(dividerColor)
             .opacity(0.6)
             .shadow(
