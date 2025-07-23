2bccf7c2b014264fa2fe1d01f35084a9ddbc32b9
Manabu Nakazawa <mshibanami+git@gmail.com>
2025-07-23 18:35:10 +1000

    Make the implementation safer to fix potential bugs
    - No more GeometryReader in the main view tree.
    - Properly implement reduce() of custom preference keys.

--- Sources/EdgeFadingScrollView/CustomScrollView.swift
+++ Sources/EdgeFadingScrollView/CustomScrollView.swift
@@ -18,7 +18,7 @@ struct CustomScrollView<Content: View>: View {
     let onContentSizeChange: ((CGSize) -> Void)?
     let onSizeChange: ((CGSize) -> Void)?
     let content: () -> Content
-    
+
     init(
         _ axes: Axis.Set = .vertical,
         showsIndicators: Bool = true,
@@ -34,50 +34,68 @@ struct CustomScrollView<Content: View>: View {
         self.onSizeChange = onSizeChange
         self.content = content
     }
-    
+
     var body: some View {
-        GeometryReader { scrollViewProxy in
-            ScrollView(axes, showsIndicators: showsIndicators) {
-                offsetReader
-                AnyView(content()) // HACK: Wrap this with AnyView() or whatever. Otherwise, the calculation of the content size doesn't work well.
-                    .padding(.top, -8)
-                    .background(
-                        GeometryReader { contentViewProxy in
-                            Color.clear
-                                .preference(key: ContentSizePreferenceKey.self, value: contentViewProxy.size)
-                                .onPreferenceChange(ContentSizePreferenceKey.self, perform: onContentSizeChange ?? { _ in })
-                        }
-                    )
+        ScrollView(axes, showsIndicators: showsIndicators) {
+            AnyView(content()) // HACK: Wrap this with AnyView() or whatever. Otherwise, the calculation of the content size doesn't work well.
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(
+                    GeometryReader { contentViewProxy in
+                        Color.clear
+                            .preference(key: ContentSizePreferenceKey.self, value: contentViewProxy.size)
+                            .onPreferenceChange(ContentSizePreferenceKey.self, perform: onContentSizeChange ?? { _ in })
+                    }
+                )
+                .background(
+                    // オフセット読み取り用のGeometryReader
+                    GeometryReader { proxy in
+                        let _ = print("CustomScrollView offset: \(proxy.frame(in: .named(frameLayerCoordinateSpaceName)))")
+                        Color.clear
+                            .preference(
+                                key: RectPreferenceKey.self,
+                                value: proxy.frame(in: .named(frameLayerCoordinateSpaceName))
+                            )
+                    }
+                )
+        }
+        .coordinateSpace(name: frameLayerCoordinateSpaceName)
+        .onPreferenceChange(RectPreferenceKey.self, perform: onOffsetChange ?? { _ in })
+        .background(
+            // ScrollViewのサイズ読み取り用のGeometryReader
+            GeometryReader { scrollViewProxy in
+                Color.clear
+                    .preference(key: SizePreferenceKey.self, value: scrollViewProxy.size)
+                    .onPreferenceChange(SizePreferenceKey.self, perform: onSizeChange ?? { _ in })
             }
-            .coordinateSpace(name: frameLayerCoordinateSpaceName)
-            .onPreferenceChange(RectPreferenceKey.self, perform: onOffsetChange ?? { _ in })
-            .preference(key: SizePreferenceKey.self, value: scrollViewProxy.size)
-            .onPreferenceChange(SizePreferenceKey.self, perform: onSizeChange ?? { _ in })
-        }
-    }
-    
-    var offsetReader: some View {
-        GeometryReader { proxy in
-            Color.clear.preference(
-                key: RectPreferenceKey.self,
-                value: proxy.frame(in: .named(frameLayerCoordinateSpaceName)))
-        }
-        .frame(height: 0)
-        // this makes sure that the reader doesn't affect the content height
+        )
     }
 }
 
 private struct RectPreferenceKey: PreferenceKey {
     static var defaultValue: CGRect = .zero
-    static func reduce(value: inout CGRect, nextValue: () -> CGRect) {}
+    static func reduce(value: inout CGRect, nextValue: () -> CGRect) {
+        value = value.union(nextValue())
+    }
 }
 
 private struct ContentSizePreferenceKey: PreferenceKey {
     static var defaultValue: CGSize = .zero
-    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+        let next = nextValue()
+        value = CGSize(
+            width: max(value.width, next.width),
+            height: max(value.height, next.height)
+        )
+    }
 }
 
 private struct SizePreferenceKey: PreferenceKey {
     static var defaultValue: CGSize = .zero
-    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {}
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+        let next = nextValue()
+        value = CGSize(
+            width: max(value.width, next.width),
+            height: max(value.height, next.height)
+        )
+    }
 }
